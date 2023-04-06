e066363d7159b1d70ddc41f8ea4aac9b71006161
Yann Le Coz <ylcoz@icloud.com>
2023-04-06 14:49:25 +0200

    feat(holiday): Holiday Controller
    Implementation Holiday controller with routes documentation.

---
+++ src/modules/holidays/holidays.controller.ts
@@ -0,0 +1,131 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+
+import { HolidaysService } from './holidays.service';
+import { CreateHolidayDto } from './dto/createHoliday.dto';
+import { UpdateHolidayDto } from './dto/updateHoliday.dto';
+import { Holiday } from './holiday.schema';
+
+import { AccessTokenGuard } from '../../guards/accessToken.guard';
+import { Roles } from '../../decorators/roles.decorator';
+import { Role } from '../../enums/role.enum';
+
+@ApiTags('holidays')
+@Controller('holidays')
+export class HolidaysController {
+  constructor(private readonly holidaysService: HolidaysService) {}
+
+  @UseGuards(AccessTokenGuard)
+  @Roles(Role.ADMINISTRATOR, Role.MANAGER, Role.EDUCATOR)
+  @ApiOperation({ summary: 'Take a vacation' })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Holiday successfully taked',
+    type: Holiday,
+  })
+  @ApiResponse({
+    status: HttpStatus.UNPROCESSABLE_ENTITY,
+    description: 'Unprocessable Entity',
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description:
+      'Unauthorized because only **Administrators**, **Managers** and **Educators** can take vacations',
+  })
+  @Post()
+  async create(@Body() createHolidayDto: CreateHolidayDto): Promise<Holiday> {
+    return await this.holidaysService.create(createHolidayDto);
+  }
+
+  @UseGuards(AccessTokenGuard)
+  @Roles(Role.ADMINISTRATOR)
+  @ApiOperation({ summary: 'Find all holidays' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'List of holidays',
+    type: [Holiday],
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description:
+      'Unauthorized because only **Administrators** can find all holidays',
+  })
+  @Get()
+  async findAll(): Promise<Holiday[]> {
+    return await this.holidaysService.findAll();
+  }
+
+  @UseGuards(AccessTokenGuard)
+  @Roles(Role.ADMINISTRATOR, Role.MANAGER, Role.EDUCATOR)
+  @ApiOperation({ summary: 'Find a holiday' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'The found holiday',
+    type: Holiday,
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: '**Client** not allowed to find a holiday',
+  })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'Not found' })
+  @Get(':holidayId')
+  async findOne(@Param('holidayId') holidayId: string): Promise<Holiday> {
+    return await this.holidaysService.findOne(holidayId);
+  }
+
+  @UseGuards(AccessTokenGuard)
+  @Roles(Role.ADMINISTRATOR, Role.MANAGER, Role.EDUCATOR)
+  @ApiOperation({ summary: 'Update a holiday' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'The modified holiday',
+    type: Holiday,
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: '**Client** not allowed to modify a holiday',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_MODIFIED,
+    description: 'Not Modified',
+  })
+  @Put(':holidayId')
+  async updateOne(
+    @Param('holidayId') holidayId: string,
+    @Body() updateHolidayDto: UpdateHolidayDto,
+  ): Promise<Holiday> {
+    return await this.holidaysService.updateOne(holidayId, updateHolidayDto);
+  }
+
+  @UseGuards(AccessTokenGuard)
+  @Roles(Role.ADMINISTRATOR, Role.MANAGER)
+  @ApiOperation({ summary: 'Delete a holiday' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'The deleted holiday',
+    type: Holiday,
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description:
+      'Unauthorized because only **Administrators** and **Managers** can delete a holiday',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Not found',
+  })
+  @Delete(':holidayId')
+  async deleteOne(@Param('holidayId') holidayId: string): Promise<Holiday> {
+    return await this.holidaysService.deleteOne(holidayId);
+  }
+}
--- src/modules/holidays/holidays.module.ts
+++ src/modules/holidays/holidays.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Holiday, HolidaySchema } from './holiday.schema';
 import { HolidaysService } from './holidays.service';
+import { HolidaysController } from './holidays.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Holiday.name, schema: HolidaySchema }]),
   ],
+  controllers: [HolidaysController],
   providers: [HolidaysService],
   exports: [HolidaysService],
 })
