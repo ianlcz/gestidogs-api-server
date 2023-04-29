465776144a6f8d47b47f78b6a0ef2baf9dd0cb04
Yann Le Coz <ylcoz@icloud.com>
2023-04-29 11:37:02 +0200

    fix(session): Find session by date (#44)
    Using $and to get only Today sessions and to Next sessions get a limit date of one year.

--- swagger-static/swagger-ui-init.js
+++ swagger-static/swagger-ui-init.js
@@ -3135,7 +3135,7 @@ window.onload = function() {
             "createdAt": {
               "format": "date-time",
               "type": "string",
-              "default": "2023-04-29T08:02:53.263Z"
+              "default": "2023-04-29T09:27:31.060Z"
             },
             "__v": {
               "type": "number"
