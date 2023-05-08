2c753e7b4d6e56589f061e543f190bec4a2506d3
Yann Le Coz <ylcoz@icloud.com>
2023-05-08 20:06:56 +0200

    src/app to app/

---
+++ src/modules/dogs/dtos/updateDog.dto.ts
@@ -0,0 +1,39 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+import { Establishment } from '../../establishments/schemas/establishment.schema';
+import { User } from '../../users/schemas/user.schema';
+import { GenderType } from '../../../common/enums/Gender.enum';
+
+export class UpdateDogDto {
+  @ApiProperty({ type: String })
+  owner: User;
+
+  @ApiProperty({ type: String })
+  establishment: Establishment;
+
+  @ApiPropertyOptional({ type: String })
+  nationalId: string;
+
+  @ApiPropertyOptional({ type: String })
+  name: string;
+
+  @ApiPropertyOptional({ type: String })
+  imageUrl: string;
+
+  @ApiPropertyOptional({
+    enum: GenderType,
+    examples: [GenderType.MALE, GenderType.FEMALE],
+  })
+  gender?: GenderType;
+
+  @ApiPropertyOptional({ type: String })
+  breed: string;
+
+  birthDate: Date;
+
+  @ApiPropertyOptional({ type: Number })
+  weight: number;
+
+  @ApiPropertyOptional({ type: Number })
+  height: number;
+}
